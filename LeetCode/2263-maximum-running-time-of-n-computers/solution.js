82158514e11d66ba4e6b85ce5c57199591c65d72
Hemank Kumar <hemank.fsu@gmail.com>
2025-12-03 18:26:02 +0000

    [LeetCode Sync] Runtime - 54 ms (73.33%), Memory - 72.9 MB (13.33%)

---
+++ LeetCode/2263-maximum-running-time-of-n-computers/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @param {number[]} batteries
+ * @return {number}
+ */
+var maxRunTime = function(n, batteries) {
+    let total = batteries.reduce((a, b) => a + b, 0);
+    let left = 0, right = Math.floor(total / n);
+
+    while (left < right) {
+        let mid = Math.floor((left + right + 1) / 2);
+        let need = mid * n;
+        let have = 0;
+
+        for (let b of batteries) {
+            have += Math.min(b, mid);
+        }
+
+        if (have >= need) left = mid;
+        else right = mid - 1;
+    }
+
+    return left;
+};
