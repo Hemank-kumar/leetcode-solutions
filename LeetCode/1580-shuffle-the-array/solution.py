75a05b3947262429866adc21755a4de85c7a999b
Hemank Kumar <hemank.fsu@gmail.com>
2025-12-04 20:31:47 +0000

    [LeetCode Sync] Runtime - 42 ms (44.17%), Memory - 12.5 MB (81.57%)

---
+++ LeetCode/1580-shuffle-the-array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shuffle(self, nums, n):
+        ans = [0] * (2 * n) 
+
+        for i in range(n):
+            ans[2 * i] = nums[i]   
+            ans[2 * i + 1] = nums[i + n]  
+
+        return ans
+
