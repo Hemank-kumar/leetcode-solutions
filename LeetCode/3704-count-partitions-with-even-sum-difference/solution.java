e3fd74e75e680a140676042c4569a2bcbec69c18
Hemank Kumar <hemank.fsu@gmail.com>
2025-12-05 12:56:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.7 MB (33.10%)

---
+++ LeetCode/3704-count-partitions-with-even-sum-difference/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int total = 0;
+        for (int x : nums) total += x;
+        int left = 0, ans = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            left += nums[i];
+            int right = total - left;
+            if (((left - right) & 1) == 0) ans++;
+        }
+        return ans;
+    }
+}
