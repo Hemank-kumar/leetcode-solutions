8f7f3d723f67f8dd25e4a09ac6333a5f0a464fd8
Hemank Kumar <hemank.fsu@gmail.com>
2026-02-05 17:36:59 +0000

    [LeetCode Sync] Runtime - 57 ms (31.16%), Memory - 57.6 MB (66.42%)

--- LeetCode/1580-shuffle-the-array/solution.js
+++ LeetCode/1580-shuffle-the-array/solution.js
@@ -4,12 +4,10 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-   let ans = new Array(2 * n);
-
-    for (let i = 0; i < n; i++) {
-        ans[2 * i] = nums[i];       // x1, x2, x3, ...
-        ans[2 * i + 1] = nums[i+n]; // y1, y2, y3, ...
+    let result = [];
+    for(let i =0 ;i<n;i++){
+        result[i*2] = nums[i];
+        result[i*2+1] = nums[i+n];
     }
-
-    return ans;
+    return result;
 };
