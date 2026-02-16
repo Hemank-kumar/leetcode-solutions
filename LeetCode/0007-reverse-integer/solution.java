100e7c13aa54da54477ce2ab0a0f5f762aba5084
Hemank Kumar <hemank.fsu@gmail.com>
2026-02-16 20:21:38 +0000

    [LeetCode Sync] Runtime - 1 ms (99.98%), Memory - 42.9 MB (11.66%)

--- LeetCode/0007-reverse-integer/solution.java
+++ LeetCode/0007-reverse-integer/solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public int reverse(int x) {
-        int rNum = 0;
-        int temp = x;
-        while(temp!=0){
-            int digit = temp%10;
-            if(rNum < Integer.MIN_VALUE / 10 || rNum > Integer.MAX_VALUE / 10){
-            return 0;
-             }
-            rNum = rNum*10 + digit;
-            temp = temp/10;
+        int rev = 0;
+        while(x != 0){
+            int digit = x%10;
+            if(rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            rev = (rev*10) + digit;
+            x = x/10;
         }
-        
-        return rNum;
+        return rev;
     }
 }
