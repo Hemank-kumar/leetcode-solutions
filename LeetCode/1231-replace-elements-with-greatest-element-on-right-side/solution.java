ed7d0731468a857ef12a3f72d37289157342084e
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-07 18:18:04 +0000

    [LeetCode Sync] Runtime - 2 ms (99.59%), Memory - 49.1 MB (65.29%)

---
+++ LeetCode/1231-replace-elements-with-greatest-element-on-right-side/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int len = arr.length;
+        int max = -1;
+        for(int i = len-1;i>=0;i--){
+            int curVal = arr[i];
+            arr[i] = max;
+            if(max < curVal){
+                max = curVal;
+            }
+        }
+        return arr;
+    }
+}
