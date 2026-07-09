fc4d410b79f2c65efe76917af080f4821b79886c
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-09 08:54:50 +0000

    [LeetCode Sync] Runtime - 2 ms (99.53%), Memory - 47.1 MB (53.72%)

---
+++ LeetCode/2398-check-if-matrix-is-x-matrix/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int len = grid.length;
+        for(int i=0;i<len; i++){
+            for(int j=0;j<len;j++){
+                boolean isDiagonal = i == j || i + j == len-1;
+                if(isDiagonal){
+                    if(grid[i][j] == 0)
+                        return false;
+                }
+                else{
+                    if(grid[i][j] != 0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
