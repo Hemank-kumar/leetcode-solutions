a3557d68747ff8c62ba0cb0cef65693c1bb64a6a
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-09 08:54:01 +0000

    [LeetCode Sync] Runtime - 2 ms (68.09%), Memory - 56.9 MB (72.34%)

---
+++ LeetCode/2398-check-if-matrix-is-x-matrix/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var checkXMatrix = function(grid) {
+    const len = grid.length;
+        for(let i=0;i<len; i++){
+            for(let j=0;j<len;j++){
+                const isDiagonal = i === j || i + j === len-1;
+                if(isDiagonal){
+                    if(grid[i][j] === 0)
+                        return false;
+                }
+                else{
+                    if(grid[i][j] !== 0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+};
