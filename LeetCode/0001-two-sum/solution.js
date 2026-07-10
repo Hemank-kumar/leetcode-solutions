e69951d5e0b818d82ce73d633b1b30d5d647b82c
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-10 14:04:57 +0000

    [LeetCode Sync] Runtime - 48 ms (14.55%), Memory - 54.1 MB (83.42%)

--- LeetCode/0001-two-sum/solution.js
+++ LeetCode/0001-two-sum/solution.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = {};
-        for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i];
-        if (map[diff] !== undefined) {
-            return [map[diff], i];
+    const len = nums.length;
+        for(let i = 0;i<len;i++){
+            for(let j = i+1;j<len;j++){
+                if(nums[i] + nums[j] == target){
+                    return [i,j];
+                }
+            }
         }
-         map[nums[i]] = i;
-    }
 };
-
