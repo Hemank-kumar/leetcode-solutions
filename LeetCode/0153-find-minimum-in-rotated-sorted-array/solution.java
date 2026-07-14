86e6743d96d3759e7614c9ff5867ae27f5c41892
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-14 18:31:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (85.88%)

--- LeetCode/0153-find-minimum-in-rotated-sorted-array/solution.java
+++ LeetCode/0153-find-minimum-in-rotated-sorted-array/solution.java
@@ -1,6 +1,17 @@
 class Solution {
+    // recursice binary search
     public int findMin(int[] nums) {
-        Arrays.sort(nums);
-        return nums[0];
+        return search(nums,0,nums.length-1);
+    }
+    private int search(int[] nums,int left,int right){
+        if(left == right){
+            return nums[left];
+        }
+        int mid = left + (right-left)/2;
+        if(nums[mid] > nums[right]){
+            return search(nums,mid+1,right);
+        }else{
+            return search(nums,left,mid);
+        }
     }
 }
