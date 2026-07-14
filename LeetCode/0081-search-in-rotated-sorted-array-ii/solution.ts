dbd05fd0346b7f354f369d6210cb7a6b6e377330
Hemank Kumar <hemank.fsu@gmail.com>
2026-07-14 15:04:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.9 MB (69.23%)

---
+++ LeetCode/0081-search-in-rotated-sorted-array-ii/solution.ts
@@ -0,0 +1,29 @@
+function search(nums: number[], target: number): boolean {
+    let left = 0;
+        let right = nums.length-1;
+        while(left <= right){
+            let mid = left + Math.floor((right - left)/2);
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[left] == nums[mid] && nums[right] == nums[mid]){
+                left++;
+                right--;
+                continue;
+            }
+             if(nums[left] <= nums[mid]){
+                if(target >= nums[left] && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if(target > nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+};
