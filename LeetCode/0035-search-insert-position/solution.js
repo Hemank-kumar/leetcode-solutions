71bfe41425eabd5e3b732f4bc3de94e362940140
Hemank Kumar <hemank.fsu@gmail.com>
2026-08-03 14:18:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 54.2 MB (28.29%)

--- LeetCode/0035-search-insert-position/solution.js
+++ LeetCode/0035-search-insert-position/solution.js
@@ -1,18 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+      let low = 0;
+      let high = nums.length - 1;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
+      while(low <= high){
+        let mid = Math.floor((low + high)/2);
+        if(nums[mid] == target){
             return mid;
-        } else if (nums[mid] > target) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
         }
-    }
-
-    return left;    
+        else if(nums[mid] > target){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+      }
+      return low;  
 };
